c053c3b870adfa35966857f4b72d00bbd52e6560
Amanda Zimmermann de Araujo <amanda.zdar@gmail.com>
2025-10-30 21:12:49 -0300

    Incrementando tela Cliente

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,63 +10,116 @@ public class Main {
         Cliente cliente = new Cliente();
         Scanner ler = new Scanner(System.in);
 
-        String tipoUsuario, usuario, senha, confirmaSenha, nome;
-        String[] resposta = new String[2];
-        int opcao, idProfissional, horarioProfissional;
-        boolean buscarProfissional;
+        String tipoUsuario, usuario, senha, confirmaSenha, nome, nomeProfissional;
+        String[] resposta;
 
-        System.out.println("Escolha uma opção:");
-        System.out.println("1-Realizar login / 2-Cadastrar usuário");
-        opcao = ler.nextInt();
+        int opcao, opcaoCliente, idProfissional, horarioProfissional;
+        boolean buscarProfissional, cadastrarUsuario, rodarSistema;
 
-        if (opcao == 1) {
-            System.out.println("Usuário: ");
-            usuario = ler.next();
-            System.out.println("Senha: ");
-            senha = ler.next();
+        rodarSistema = true;
 
-            resposta = login.realizarLogin(usuario,senha);
-            System.out.println(resposta[0]);
+        while(rodarSistema) {
 
-            if (resposta[1].equals("CLIENTE")) {
+            System.out.println("Escolha uma opção:");
+            System.out.println("1-Realizar login / 2-Cadastrar usuário / 3-Fechar");
+            opcao = ler.nextInt();
 
-                buscarProfissional = true;
+            switch (opcao) {
+                case 1:
+                    System.out.println("Usuário: ");
+                    usuario = ler.next();
+                    System.out.println("Senha: ");
+                    senha = ler.next();
 
-                while (buscarProfissional) {
-                    resposta = cliente.buscarProfissional("L");
-
+                    resposta = login.realizarLogin(usuario, senha);
                     System.out.println(resposta[0]);
-                    idProfissional = ler.nextInt();
 
-                    if (idProfissional != 0) {
-                        resposta = cliente.selecionarProfissional(idProfissional);
+                    if (resposta[1].equals("CLIENTE")) {
+
+                        buscarProfissional = true;
+
+                        while (buscarProfissional) {
+
+                            System.out.println("Digite o nome do profissional: ");
+                            nomeProfissional = ler.next();
+
+                            resposta = cliente.buscarProfissional(nomeProfissional);
+
+                            //Profissional encontrado
+                            if (resposta[0] != null) {
+
+                                //Escolhe horário
+                                System.out.println(resposta[0]);
+                                idProfissional = ler.nextInt();
+
+                                if (idProfissional != 0) {
+                                    resposta = cliente.selecionarProfissional(idProfissional);
+                                    System.out.println(resposta[0]);
+                                    horarioProfissional = ler.nextInt();
+
+                                    if (horarioProfissional != 0) {
+
+                                        resposta = cliente.agendarConsulta(idProfissional, horarioProfissional);
+                                        System.out.println(resposta[0]);
+
+                                        System.out.println("0-Voltar para login / 1-Visualizar consultas cadastradas / 2-Cadastrar outra consulta");
+                                        opcaoCliente = ler.nextInt();
+
+                                        switch (opcaoCliente) {
+                                            case 1:
+                                                buscarProfissional = false;
+                                                break;
+
+                                            case 2:
+                                                // Lógica para visualizar consultas
+                                                break;
+
+                                            default:
+                                                System.out.println("Opção inválida!");
+                                                break;
+                                        }
+
+                                    }
+
+                                }
+                            } else System.out.println("Profissional não encontrado...");
+                            nomeProfissional = "";
+                        }
+                    }
+
+                    break;
+
+                case 2:
+                    cadastrarUsuario = true;
+
+                    while (cadastrarUsuario) {
+                        System.out.println("Escolha o tipo de usuário(CLIENTE / ADM): ");
+                        tipoUsuario = ler.next();
+                        System.out.println("Digite seu nome completo: ");
+                        nome = ler.next();
+                        System.out.println("Cadastre um suário: ");
+                        usuario = ler.next();
+                        System.out.println("Cadastre uma senha: ");
+                        senha = ler.next();
+                        System.out.println("Confirme sua senha: ");
+                        confirmaSenha = ler.next();
+
+                        resposta = login.cadastrarUsuario(tipoUsuario, nome, usuario, senha, confirmaSenha);
                         System.out.println(resposta[0]);
-                        horarioProfissional = ler.nextInt();
 
-                        if (horarioProfissional != 0) {
+                        if (resposta[2].equals("CADASTRADO")) {
+                            cadastrarUsuario = false;
+                        }
+                    }
 
-                            resposta = cliente.agendarConsulta(idProfissional, horarioProfissional);
-                            System.out.println(resposta[0]);
-                        } else buscarProfissional = false;
+                    break;
 
-                    } else buscarProfissional = false;
-                }
+                case 3:
+                    System.out.println("Fechando...");
+                    rodarSistema = false;
+
+                    break;
             }
-
-        } else if (opcao == 2) {
-            System.out.println("Escolha o tipo de usuário(CLIENTE / ADM): ");
-            tipoUsuario = ler.next();
-            System.out.println("Digite seu nome completo: ");
-            nome = ler.next();
-            System.out.println("Cadastre um suário: ");
-            usuario = ler.next();
-            System.out.println("Cadastre uma senha: ");
-            senha = ler.next();
-            System.out.println("Confirme sua senha: ");
-            confirmaSenha = ler.next();
-
-            resposta = login.cadastrarUsuario(tipoUsuario, nome, usuario, senha, confirmaSenha);
-            System.out.println(resposta[0]);
         }
 
     }
